04244807690d3db6fecb142231a6ca032524f916
skyrob <skyrob@hotmail.com>
2016-07-11 17:41:30 +0200

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Bonjour Bis"
+puts "I like typing this"
+puts "C'est fun et cool"
+puts "Yay! Printing"
+puts "Je préfèrerais que tu ne le fasses pas
+puts 'I "said" do not touch this.'
+puts "Différence entre ' et " "
